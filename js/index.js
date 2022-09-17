6d45e226e5ba553b79e11390940ca2b092829055
Vicente <vmedina@omitsis.com>
2022-09-17 11:56:47 +0200

    :sparkles: iteration 2 done!

--- js/index.js
+++ js/index.js
@@ -2,15 +2,27 @@ new Vue({
   el: '#card',
   data() {
     return {
-      name: 'Ironhack Rubber Duck',
-      amount: 3,
-      price: 25,
-      total: 0
+      items: [
+        {
+          name: 'Ironhack Rubber Duck',
+          amount: 1,
+          price: 25,
+          total: 0
+        },
+        {
+          name: 'Ironhack Beach Towel',
+          amount: 1,
+          price: 12.5,
+          total: 0
+        }
+      ]
     };
   },
   methods: {
     calculate() {
-      this.total = this.amount * this.price;
-    },
+      this.items.forEach((item) => {
+        item.total = item.amount * item.price;
+      });
+    }
   }
 });
