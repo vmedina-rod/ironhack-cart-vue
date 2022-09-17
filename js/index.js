bd90a2a12b463581e18beb7770caa1c37fdc60e6
Vicente <vmedina@omitsis.com>
2022-09-17 11:26:11 +0200

    :sparkles: first iteration done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 new Vue({
-  el: '#card'
-})
+  el: '#card',
+  data() {
+    return {
+      name: 'Ironhack Rubber Duck',
+      amount: 3,
+      price: 25,
+      total: 0
+    };
+  },
+  methods: {
+    calculate() {
+      this.total = this.amount * this.price;
+    },
+  }
+});
